61a9b0d1ffbe81da568dc6f6ce47aa4d3e9bb4c0
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2021-12-22 17:42:35 -0800

    Add files via upload

---
+++ cah.php
@@ -0,0 +1,282 @@
+<?php
+
+$cards[0] = 'Hey Reddit! I am __________.
+Ask me anything.';
+$cards[1] = 'Introducing
+X-treme Baseball!
+It is like baseball,
+but with __________!';
+$cards[2] = 'Next from J.K.
+Rowling: Harry
+Potter and the
+Chamber of
+__________.';
+$cards[3] = "That's right, I killed
+__________.
+How, you ask?
+__________.";
+$cards[4] = "Dude, do not go
+in that bathroom.
+There's __________
+in there.";
+ 
+$cards[5] = "__________. Betcha can't have
+just one!";
+ 
+$cards[6] = "Kids, I don't need
+drugs to get high.
+I'm high on
+__________.";
+ 
+$cards[7] = 'In the new Disney
+Channel Original
+Movie, Hannah
+Montana struggles
+with __________
+for the first time.';
+ 
+$cards[8] = 'What is my
+secret power?';
+ 
+$cards[9] = "What is Batman's
+guilty pleasure?";
+ 
+$cards[10] = 'TSA guidelines now
+prohibit __________
+on airplanes.';
+ 
+$cards[11] = "I'm sorry,
+Professor, but I
+couldn't complete
+my homework
+because of
+__________.";
+ 
+$cards[12] = 'And the Academy
+Award for __________
+goes to
+__________.';
+ 
+$cards[13] = "It's a pity that kids
+these days are all
+getting involved
+with __________.";
+ 
+$cards[14] = 'Step 1: __________.
+Step 2: __________.
+Step 3: Profit.';
+ 
+$cards[15] = 'For my next trick, I
+will pull __________
+out of __________.';
+ 
+$cards[16] = '?
+I’m going on a
+cleanse this week.
+Nothing but kale
+juice and __________.';
+ 
+$cards[17] = '__________ + __________
+= __________.';
+ 
+$cards[18] = 'When Pharaoh
+remained unmoved,
+Moses called down
+a Plague of
+__________.';
+ 
+$cards[19] = 'Just once, I would like
+to hear you say
+“Thanks, Mom.
+Thanks for
+__________.”';
+ 
+$cards[20] = '50% of all
+marriages end in
+__________.';
+ 
+$cards[21] = 'My fellow
+Americans: Before
+this decade is
+out, we will have __________
+on the moon!';
+ 
+$cards[22] = 'Life for American
+Indians was forever
+changed when
+the White Man
+introduced them to
+__________.';
+ 
+$cards[23] = 'What is Teach For
+America using
+to inspire inner
+city students to
+succeed?';
+ 
+$cards[24] = '__________ is a slippery slope
+that leads to
+__________.';
+ 
+$cards[25] = 'Daddy, why is
+mommy crying?';
+ 
+$cards[26] = 'When I was tripping
+on acid,
+turned into
+__________.';
+ 
+$cards[27] = "This season at
+Steppenwolf,
+Samuel Beckett's
+classic existential
+play: Waiting for
+__________.";
+ 
+$cards[28] = 'Instead of coal,
+Santa now gives
+the bad children
+__________.';
+ 
+$cards[29] = "Maybe she's born
+with it. Maybe it's
+__________.";
+ 
+$cards[30] = 'What is George
+W. Bush thinking
+about right now?';
+ 
+$cards[31] = 'Why do I hurt
+all over?';
+ 
+$cards[32] = 'A romantic,
+candlelit dinner
+would be
+incomplete without
+__________.';
+ 
+$cards[33] = 'The class field trip
+was completely
+ruined by
+__________.';
+ 
+$cards[34] = "What's a girl's
+best friend?";
+ 
+$cards[35] = "Dear Abby, I'm
+having some
+trouble with __________
+and would like
+your advice.";
+ 
+$cards[36] = "In Jordan Peele's new
+thriller, a young family
+discovers that __________
+had really been __________
+all along.";
+ 
+$cards[37] = 'What never fails to
+liven up the party?';
+ 
+$cards[38] = 'IF you like
+__________,
+YOU MIGHT BE
+A REDNECK.';
+ 
+$cards[39] = 'Hey guys, welcome
+to Chilis! Would
+you like to start the
+night off right with
+__________?';
+ 
+$cards[40] = 'I got 99 problems
+but __________
+aint one.';
+ 
+$cards[41] = 'What would
+grandma find
+disturbing, yet
+oddly charming?';
+ 
+$cards[42] = '__________. That was so metal.';
+ 
+$cards[43] = 'What is that sound?';
+ 
+$cards[44] = 'When I am
+President, I
+will create the
+Department of
+.';
+ 
+$cards[45] = 'What are my
+parents hiding
+from me?';
+ 
+$cards[46] = 'Make a haiku.';
+ 
+$cards[47] = 'What made my first
+kiss so awkward?';
+ 
+$cards[48] = "__________. It's a trap!";
+ 
+$cards[49] = "Hulu's new reality
+show features
+twelve hot singles
+living with
+__________.";
+ 
+$cards[50] = 'I never truly
+understood
+until I encountered
+__________.';
+ 
+$cards[51] = 'During sex, I like
+to think about
+__________.';
+ 
+$cards[52] = "Uh, hey guys, I
+know this was my
+idea, but I'm having
+serious doubts
+about __________.";
+ 
+$cards[53] = 'Why am I sticky?';
+ 
+$cards[54] = "I'm no doctor,
+but I'm pretty
+sure what you're
+suffering from
+is called
+'__________'.";
+ 
+$cards[55] = 'Lifetime® presents
+“ :
+the Story of
+.”';
+ 
+$cards[56] = 'What is that smell?';
+ 
+$cards[57] = 'Why is Brett
+so sweaty?';
+ 
+$cards[58] = 'Here is the church
+Here is the steeple
+Open the doors
+And there is
+__________.';
+ 
+$cards[59] = 'But before I kill
+you, Mr. Bond, I
+must show you
+__________.';
+ 
+$cards[60] = 'Next on ESPN2:
+The World Series of
+__________.';
+ 
+
+
+$i = rand(0, 60);
+echo $cards[$i];
+
+?>
