a580f0f87975c001f9a2caf3733c319cd9fc6356
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2021-12-22 17:35:59 -0800

    Add files via upload

--- cah-black.php
+++ cah-black.php
@@ -19,264 +19,264 @@ $cards[4] = "Dude, do not go
 in that bathroom.
 There's __________
 in there.";
-echo '<br>';
+ 
 $cards[5] = "__________. Betcha can't have
 just one!";
-echo '<br>';
+ 
 $cards[6] = "Kids, I don't need
 drugs to get high.
 I'm high on
 __________.";
-echo '<br>';
+ 
 $cards[7] = 'In the new Disney
 Channel Original
 Movie, Hannah
 Montana struggles
 with __________
 for the first time.';
-echo '<br>';
+ 
 $cards[8] = 'What is my
 secret power?';
-echo '<br>';
+ 
 $cards[9] = "What is Batman's
 guilty pleasure?";
-echo '<br>';
+ 
 $cards[10] = 'TSA guidelines now
 prohibit __________
 on airplanes.';
-echo '<br>';
+ 
 $cards[11] = "I'm sorry,
 Professor, but I
 couldn't complete
 my homework
 because of
 __________.";
-echo '<br>';
+ 
 $cards[12] = 'And the Academy
 Award for __________
 goes to
 __________.';
-echo '<br>';
+ 
 $cards[13] = "It's a pity that kids
 these days are all
 getting involved
 with __________.";
-echo '<br>';
+ 
 $cards[14] = 'Step 1: __________.
 Step 2: __________.
 Step 3: Profit.';
-echo '<br>';
+ 
 $cards[15] = 'For my next trick, I
 will pull __________
 out of __________.';
-echo '<br>';
+ 
 $cards[16] = '?
 I’m going on a
 cleanse this week.
 Nothing but kale
 juice and __________.';
-echo '<br>';
+ 
 $cards[17] = '__________ + __________
 = __________.';
-echo '<br>';
+ 
 $cards[18] = 'When Pharaoh
 remained unmoved,
 Moses called down
 a Plague of
 __________.';
-echo '<br>';
+ 
 $cards[19] = 'Just once, I would like
 to hear you say
 “Thanks, Mom.
 Thanks for
 __________.”';
-echo '<br>';
+ 
 $cards[20] = '50% of all
 marriages end in
 __________.';
-echo '<br>';
+ 
 $cards[21] = 'My fellow
 Americans: Before
 this decade is
 out, we will have __________
 on the moon!';
-echo '<br>';
+ 
 $cards[22] = 'Life for American
 Indians was forever
 changed when
 the White Man
 introduced them to
 __________.';
-echo '<br>';
+ 
 $cards[23] = 'What is Teach For
 America using
 to inspire inner
 city students to
 succeed?';
-echo '<br>';
+ 
 $cards[24] = '__________ is a slippery slope
 that leads to
 __________.';
-echo '<br>';
+ 
 $cards[25] = 'Daddy, why is
 mommy crying?';
-echo '<br>';
+ 
 $cards[26] = 'When I was tripping
 on acid,
 turned into
 __________.';
-echo '<br>';
+ 
 $cards[27] = "This season at
 Steppenwolf,
 Samuel Beckett's
 classic existential
 play: Waiting for
 __________.";
-echo '<br>';
+ 
 $cards[28] = 'Instead of coal,
 Santa now gives
 the bad children
 __________.';
-echo '<br>';
+ 
 $cards[29] = "Maybe she's born
 with it. Maybe it's
 __________.";
-echo '<br>';
+ 
 $cards[30] = 'What is George
 W. Bush thinking
 about right now?';
-echo '<br>';
+ 
 $cards[31] = 'Why do I hurt
 all over?';
-echo '<br>';
+ 
 $cards[32] = 'A romantic,
 candlelit dinner
 would be
 incomplete without
 __________.';
-echo '<br>';
+ 
 $cards[33] = 'The class field trip
 was completely
 ruined by
 __________.';
-echo '<br>';
+ 
 $cards[34] = "What's a girl's
 best friend?";
-echo '<br>';
+ 
 $cards[35] = "Dear Abby, I'm
 having some
 trouble with __________
 and would like
 your advice.";
-echo '<br>';
+ 
 $cards[36] = "In Jordan Peele's new
 thriller, a young family
 discovers that __________
 had really been __________
 all along.";
-echo '<br>';
+ 
 $cards[37] = 'What never fails to
 liven up the party?';
-echo '<br>';
+ 
 $cards[38] = 'IF you like
 __________,
 YOU MIGHT BE
 A REDNECK.';
-echo '<br>';
+ 
 $cards[39] = 'Hey guys, welcome
 to Chilis! Would
 you like to start the
 night off right with
 __________?';
-echo '<br>';
+ 
 $cards[40] = 'I got 99 problems
 but __________
 aint one.';
-echo '<br>';
+ 
 $cards[41] = 'What would
 grandma find
 disturbing, yet
 oddly charming?';
-echo '<br>';
+ 
 $cards[42] = '__________. That was so metal.';
-echo '<br>';
+ 
 $cards[43] = 'What is that sound?';
-echo '<br>';
+ 
 $cards[44] = 'When I am
 President, I
 will create the
 Department of
 .';
-echo '<br>';
+ 
 $cards[45] = 'What are my
 parents hiding
 from me?';
-echo '<br>';
+ 
 $cards[46] = 'Make a haiku.';
-echo '<br>';
+ 
 $cards[47] = 'What made my first
 kiss so awkward?';
-echo '<br>';
+ 
 $cards[48] = "__________. It's a trap!";
-echo '<br>';
+ 
 $cards[49] = "Hulu's new reality
 show features
 twelve hot singles
 living with
 __________.";
-echo '<br>';
+ 
 $cards[50] = 'I never truly
 understood
 until I encountered
 __________.';
-echo '<br>';
+ 
 $cards[51] = 'During sex, I like
 to think about
 __________.';
-echo '<br>';
+ 
 $cards[52] = "Uh, hey guys, I
 know this was my
 idea, but I'm having
 serious doubts
 about __________.";
-echo '<br>';
+ 
 $cards[53] = 'Why am I sticky?';
-echo '<br>';
+ 
 $cards[54] = "I'm no doctor,
 but I'm pretty
 sure what you're
 suffering from
 is called
 '__________'.";
-echo '<br>';
+ 
 $cards[55] = 'Lifetime® presents
 “ :
 the Story of
 .”';
-echo '<br>';
+ 
 $cards[56] = 'What is that smell?';
-echo '<br>';
+ 
 $cards[57] = 'Why is Brett
 so sweaty?';
-echo '<br>';
+ 
 $cards[58] = 'Here is the church
 Here is the steeple
 Open the doors
 And there is
 __________.';
-echo '<br>';
+ 
 $cards[59] = 'But before I kill
 you, Mr. Bond, I
 must show you
 __________.';
-echo '<br>';
+ 
 $cards[60] = 'Next on ESPN2:
 The World Series of
 __________.';
-echo '<br>';
+ 
 
 
 $i = rand(0, 60);
-echo $cards[$i];
+echo '<h2>'$cards[$i]'</h2>';
 
 ?>
